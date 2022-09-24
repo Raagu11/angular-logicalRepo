bade4848cb70ae23f54d4ea3626e326d74430668
Raagu11 <106811919+Raagu11@users.noreply.github.com>
2022-09-24 16:27:16 +0530

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component, VERSION } from '@angular/core';
+import { CountryInterface } from './countryinterface';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  countryList: CountryInterface[] = [
+    {
+      id: 1,
+      countryName: 'india',
+      countryCode: '+91',
+    },
+    {
+      id: 2,
+      countryName: 'uk',
+      countryCode: '+95',
+    },
+    {
+      id: 3,
+      countryName: 'austria',
+      countryCode: '+99',
+    },
+    {
+      id: 4,
+      countryName: 'japan',
+      countryCode: '+96',
+    },
+  ];
 }
