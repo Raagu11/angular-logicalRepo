d236efcfe39a423b7eb6cb0dece19c099a5e19f7
Raagu11 <106811919+Raagu11@users.noreply.github.com>
2022-09-28 10:06:00 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserListComponent } from './Services/user-list/user-list.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, UserListComponent],
   bootstrap: [AppComponent],
   providers: [],
 })
